4ed55073b9b5f9fb7b48d0e67f6e0b079c73c2ad
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-12 00:25:28 +0530

    Test with empty string and one number

--- StringCalculator/src/com/stringcalculator/StringCalculator.java
+++ StringCalculator/src/com/stringcalculator/StringCalculator.java
@@ -2,4 +2,12 @@ package com.stringcalculator;
 
 public class StringCalculator {
 
+	public int add(String input) {
+		if(input.equals("")) {
+			return 0;
+		}else {
+			return Integer.parseInt(input);
+		}
+	}
+
 }
--- StringCalculator/test/com/stringcalculator/StringCalculatorTest.java
+++ StringCalculator/test/com/stringcalculator/StringCalculatorTest.java
@@ -2,13 +2,22 @@ package com.stringcalculator;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyString() {
+		StringCalculator cal = new StringCalculator();
+		assertEquals(0, cal.add(""));
 	}
-
+	
+	@Test
+	public void testOneNumber() {
+		StringCalculator cal = new StringCalculator();
+		assertEquals(1, cal.add("1"));
+		assertEquals(2, cal.add("2"));
+		assertEquals(3, cal.add("3"));
+	}
 }
